23592b09ce9cad66e61d81b63bbdcfcf1ff3dcc7
LAICHEEHOONG <47762131+LAICHEEHOONG@users.noreply.github.com>
2022-10-11 14:26:10 +0800

    Update server.js
    __dirname, 'build'

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const fileUpload = require('express-fileupload');
 const cors = require('cors');
-require('dotenv').config(); 
+require('dotenv').config();
 const mongoUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_COLLECTION}?retryWrites=true&w=majority`;
 const path = require('path');
 const userRoute = require('./routes/api/user');
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(fileUpload());
 app.use(cors({
-  origin: '*'
+    origin: '*'
 }));
 
 
@@ -35,44 +35,32 @@ app.use('/api/list', listRoute);
 app.use('/api/input', inputRoute);
 app.use('/api/history', historyRoute);
 
-/************ heroku **********/
-app.use(express.static('build'));
+
+
+/************ render.com **********/
+app.use(express.static(path.join(__dirname, 'build')));
 
 if (process.env.NODE_ENV === 'production') {
-  app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, '../index.html'), function (err) {
-      if (err) {
-        res.status(500).send(err)
-      }
-    });
-  })
-}
+    app.get('/*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'build', 'index.html'), function (err) {
+            if (err) {
+                res.status(500).send({ error: err, message: 'production' })
+            }
+        });
+    })
+};
 
 
 
-/********* 检查mongoDB 链接 *********/
-// MongoClient.connect(mongoUri, (err, client) => {
-//   if(err) {
-//     throw err;
-//   }
-//   console.log('connected to the db');
-// })
+
+
+
 
 const port = process.env.PORT || 3001;
 
+
+
 app.listen(port, () => {
-  console.log(`Connect port: ${port}`)
+    console.log(`Connect port: ${port}`)
 })
 
-//mongo db
-//username laicheehoong 
-//password 0175337331
-
-//rm -rf .git // cd client
-
-//heroku login
-//git add .
-//git commit -am ""
-//git push heroku master
-
-//https://stockcheck-pcd-us.herokuapp.com/
