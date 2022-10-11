f0a95f0119b736f332334faede840dfc61747cec
LAICHEEHOONG <47762131+LAICHEEHOONG@users.noreply.github.com>
2022-10-11 15:47:58 +0800

    Update server.js
    back to ori

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ if (process.env.NODE_ENV === 'production') {
   app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, '../build/index.html'), function (err) {
       if (err) {
-        res.status(500).send({error:err, message: 'production RENDER.COM'})
+        res.status(500).send({error:err, message: 'nnn'})
       }
     });
   })
