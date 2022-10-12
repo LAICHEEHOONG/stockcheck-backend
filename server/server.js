d7a2444e0521b9919142c70b1306b428ee96c991
LAICHEEHOONG <47762131+LAICHEEHOONG@users.noreply.github.com>
2022-10-12 11:19:45 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ if (process.env.NODE_ENV === 'production') {
   app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, '../build/index.html'), function (err) {
       if (err) {
-        res.status(500).send({error:err, message: 'other way send cookie'})
+        res.status(500).send({error:err, message: 'update user.js'})
       }
     });
   })
